0d3ab49ac03aab8ca5b5fb87f0cb9f61251905ce
Jack Laxson <jackjrabbit@gmail.com>
2019-07-10 08:56:07 -0400

    eslint for gjs amazing!

--- extension.js
+++ extension.js
@@ -5,32 +5,32 @@ const GnomeDesktop = imports.gi.GnomeDesktop
 
 let text, button, Clock
 
-function GetKilosecondsNow () {
-	//cribbed https://wiki.archlinux.org/index.php/User:Haxit/Kiloseconds#Javascript
-	ourDate = new Date()
-	metricTime =  (ourDate.getHours() * 3600 + ourDate.getMinutes() * 60 + ourDate.getSeconds())
-	return metricTime/1000
+function GetKilosecondsNow() {
+    //cribbed https://wiki.archlinux.org/index.php/User:Haxit/Kiloseconds#Javascript
+    let ourDate = new Date()
+    let metricTime =  (ourDate.getHours() * 3600 + ourDate.getMinutes() * 60 + ourDate.getSeconds())
+    return metricTime/1000
 }
 
 function on_tick() {
-	let ks = GetKilosecondsNow()
-	let displayText = new St.Label({text: "%s ks".format(ks)})
+    let ks = GetKilosecondsNow()
+    let displayText = new St.Label({text: '%s ks'.format(ks)})
     button.set_child(displayText)
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
+    let button = new St.Bin({ style_class: 'panel-button',
+        reactive: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true })
     on_tick()
 }
 
 function enable() {
     Main.panel._centerBox.insert_child_at_index(button, 0)
-    // ref  gnome-shell /gnome-shell-3.5.4/js/ui/dateMenu.js 
-    Clock = new GnomeDesktop.WallClock()
+    // ref  gnome-shell /gnome-shell-3.5.4/js/ui/dateMenu.js
+    let Clock = new GnomeDesktop.WallClock()
     Clock.connect('notify::clock', on_tick)
 }
 
