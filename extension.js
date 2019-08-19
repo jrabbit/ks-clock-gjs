bf8e65ced8da8397663e34e8a82bf4416af41828
Jack Laxson <jackjrabbit@gmail.com>
2019-08-19 01:23:35 -0400

    incorperate feedback

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ const Main = imports.ui.main
 const Tweener = imports.ui.tweener
 const GnomeDesktop = imports.gi.GnomeDesktop
 
-let text, button, Clock
+let text, button, clock_signal, Clock
 
 function GetKilosecondsNow() {
     //cribbed https://wiki.archlinux.org/index.php/User:Haxit/Kiloseconds#Javascript
@@ -18,25 +18,20 @@ function on_tick() {
     button.set_child(displayText)
 }
 
-function init() {
+function enable() {
     button = new St.Bin({ style_class: 'panel-button',
         reactive: true,
         x_fill: true,
         y_fill: false,
         track_hover: true })
-}
-
-
-function enable() {
-
     on_tick()
     Main.panel._centerBox.insert_child_at_index(button, 0)
     // ref  gnome-shell /gnome-shell-3.5.4/js/ui/dateMenu.js
-    let Clock = new GnomeDesktop.WallClock()
-    Clock.connect('notify::clock', on_tick)
+    Clock = new GnomeDesktop.WallClock()
+    clock_signal = Clock.connect('notify::clock', on_tick)
 }
 
 function disable() {
     Main.panel._centerBox.remove_child(button)
-    Clock.disconnect(on_tick)
+    Clock.disconnect(clock_signal)
 }
